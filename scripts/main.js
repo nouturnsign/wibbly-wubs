0d865a4de01195f1476bf0cb3e2b5c38f31d908a
Nikolas Brandt <nik@wumpler.com>
2024-11-11 23:09:31 -0800

    add sound visualization basics

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,31 @@
 import * as THREE from "three";
+import { getFreqData, getTimeData } from "./sound";
+
+const freqPar = document.getElementById("freq");
+const timePar = document.getElementById("time");
+
+setInterval(() => {
+  const freq = getFreqData();
+  if (freq === null) return;
+
+  let out = "";
+
+  for (let i = 0; i < freq.length; ++i) {
+    out += "|" + "F".repeat(freq[i] / 2) + "\n";
+  }
+
+  freqPar.innerText = out;
+
+  const time = getTimeData();
+
+  out = "";
+
+  for (let i = 0; i < time.length; ++i) {
+    out += "|" + " ".repeat(time[i] / 2 - 1) + "*" + "\n";
+  }
+
+  timePar.innerText = out;
+}, 1000 / 120);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -10,8 +37,8 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+// renderer.setAnimationLoop(animate);
+// document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
